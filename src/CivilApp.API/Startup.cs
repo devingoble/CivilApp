de216e3520b0cad62bdba471016384f2899e972c
Devin Goble <devin.goble@outlook.com>
2021-04-13 10:29:58 -0700

    Docker working

--- src/CivilApp.API/Startup.cs
+++ src/CivilApp.API/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -42,6 +43,10 @@ namespace CivilApp.API
             services.AddScoped(typeof(IAsyncRepository<>), typeof(EfRepository<>));
             services.AddScoped<IDateTimeService, DateTimeService>();
             services.AddScoped<IJacketSequenceService, JacketSequenceService>();
+            services.AddDbContext<JacketContext>(options =>
+            {
+                options.UseSqlServer();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
